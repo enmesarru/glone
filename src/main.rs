0546038ea105aa54211f7992e03e075f980b60a0
enmesarru <fkurutass@gmail.com>
2023-08-12 21:23:54 +0300

    added log path for logging at configuration dir

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    logger::init_logger();
-
     let mut glone_options = app::GloneOptions::new();
+    logger::init_logger(&glone_options.log_path);
     glone_options.load();
-
-    log::info!("Logger initialized");
 }
 
 pub mod app {
@@ -17,21 +14,25 @@ pub mod app {
     pub struct GloneOptions {
         config_dir_path: PathBuf,
         config_path: PathBuf,
+        pub log_path: PathBuf,
         config: Option<Config>,
     }
 
     impl GloneOptions {
         const CONFIG_PATH: &str = ".glone";
         const CONFIG_FILE: &str = "config.yaml";
+        const LOG_FILE: &str = "app.log";
 
         pub fn new() -> GloneOptions {
             let user_config_dir = config_dir().unwrap();
             let config_dir_path = Path::new(&user_config_dir).join(Self::CONFIG_PATH);
             let config_path = Path::new(&config_dir_path).join(Self::CONFIG_FILE);
+            let log_path = Path::new(&config_dir_path).join(Self::LOG_FILE);
 
             return GloneOptions {
                 config_dir_path,
                 config_path,
+                log_path,
                 config: None,
             };
         }
@@ -46,7 +47,7 @@ pub mod app {
             if !self.config_dir_path.exists() {
                 match std::fs::create_dir_all(&self.config_dir_path) {
                     Ok(_) => {
-                        log::info!("Creating an app config folder .");
+                        log::info!("Creating an app config folder.");
                     }
                     Err(_) => {
                         log::error!("Error occured when creating app config folder");
@@ -57,10 +58,10 @@ pub mod app {
         }
 
         fn create_config_file(&self) {
-            if self.config_dir_path.exists() {
+            if self.config_dir_path.exists() && !self.config_path.exists() {
                 match std::fs::File::create(&self.config_path) {
                     Ok(_) => {
-                        log::info!("Created an empty config file.");
+                        log::info!("Created an empty config file. {:?}", self.config_path);
                     }
                     Err(err) => {
                         log::error!("Error occured when creating empty config folder, {:?}", err);
@@ -74,6 +75,11 @@ pub mod app {
 
             match content {
                 Ok(c) => {
+                    if c.len() == 0 {
+                        log::error!("Error: The config file is empty.");
+                        return;
+                    }
+
                     let config: Result<Config, serde_yaml::Error> = serde_yaml::from_str(&c);
 
                     match config {
@@ -109,8 +115,8 @@ pub mod config {
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Auth {
         r#type: AuthType,
-        username: String,
-        password: String,
+        username: Option<String>,
+        password: Option<String>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
@@ -123,13 +129,15 @@ pub mod config {
 }
 
 pub mod logger {
+    use std::path::PathBuf;
+
     use log::LevelFilter;
     use log4rs::append::console::ConsoleAppender;
     use log4rs::append::file::FileAppender;
     use log4rs::config::{Appender, Config as LogConfig, Root};
     use log4rs::encode::pattern::PatternEncoder;
 
-    pub fn init_logger() {
+    pub fn init_logger(log_path: &PathBuf) {
         let stdout = ConsoleAppender::builder()
             .encoder(Box::new(PatternEncoder::new("{d} [{l}] - {m}{n}")))
             .build();
@@ -137,7 +145,7 @@ pub mod logger {
         // Create a file appender
         let file = FileAppender::builder()
             .encoder(Box::new(PatternEncoder::new("{d} [{l}] - {m}{n}")))
-            .build("log/app.log")
+            .build(log_path)
             .unwrap();
 
         let root = Root::builder()
@@ -152,5 +160,7 @@ pub mod logger {
             .unwrap();
 
         log4rs::init_config(config).unwrap();
+
+        log::info!("Logger initialized");
     }
 }
